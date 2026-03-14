eeac754e64f8af6b3293600fbf142bf546bc7118
Pratham Mewara <prathammewara7773@gmail.com>
2026-03-15 00:46:26 +0530

    Add World Bank indicator metadata extraction and dataset storage pipeline

--- python/world_bank_pipeline.py
+++ python/world_bank_pipeline.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 import time
 
+
 #---------------------------------------------World bank countries endpoint--------------------------------------------
 url =f"https://api.worldbank.org/countries?format=json&per_page=300"
 
@@ -21,6 +22,9 @@ countries = data[1]
 # convert to dataframe
 countries = pd.DataFrame(countries)
 
+#Save countries raw datasets
+countries.to_csv("../data/raw/countries_raw.csv", index=False)
+print("Countries raw dataset saved")
 
 #---------------------------------------------Cleaning countries data frame---------------------------------------------
 
@@ -34,5 +38,51 @@ countries.drop(columns = ["adminregion" , "capitalCity"], inplace = True)
 # Rename column
 countries.rename(columns = {'iso2Code' : 'country_id'} , inplace = True)
 
+# Save countries processed datasets
+#countries.to_csv("../data/processed/countries_clean.csv", index=False)
+#print("Countries cleaned dataset saved")
+
+#---------------------------------------------Getting indicators---------------------------------------------
+base_url = "https://api.worldbank.org/v2/indicator?format=json"
+response = requests.get(base_url)
+#print(response.status_code) --> 200 output means request successful
+indicators_data = response.json()
+
+# indicators_data[0] -->meta data
+#{'page': 1, 'pages': 590, 'per_page': '50', 'total': 29470}
+
+#indicators_data[1] -->Actual data
+
+all_dfs = []
+total_pages = indicators_data[0]["pages"]
+for i in range(1,total_pages+1):
+    url = f"https://api.worldbank.org/v2/indicator?format=json&per_page=500&page={i}"
+    response = requests.get(url, timeout=30)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        if len(data)<2:
+            print(f"NO data at page {i}")
+
+        indicators = data[1]
+        df = pd.DataFrame([{"id" : item['id'],
+                       "name" : item['name']} for item in indicators ])
+
+        all_dfs.append(df)
+        print(f"Page{i}: {len(df)} indicators collected")
+
+        time.sleep(0.3)
+
+    else :
+        print(f"Failed to fetch page {i}, status code {response.status_code}")
+
+
+final_df = pd.concat(all_dfs , ignore_index = True)
+print(final_df)
+
+# Save indicator metadata dataset
+final_df.to_csv("../data/raw/world_bank_indicators_metadata.csv", index=False)
+print("Indicator metadata saved")
 
 
